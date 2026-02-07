887a958602b52bd2d5cc7a2c7562dc0208b8da13
soudeh <soudeh_akmali1991@yahoo.com>
2026-02-07 20:06:04 +0100

    second commit , add some condition in each part to avoid wrong input

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,49 @@ public class Main {
     public static void main(String[] args) {
         final byte Month_of_year = 12;
         final byte Percentage = 100;
-
+        float Principal;
+        float AnnualIntrest;
+        float MonthlyIntrest;
+        byte years;
         Scanner scan = new Scanner(System.in);
 
-        // amount of principal for loan
-        System.out.println(" Principal :  ");
-        float Principal = scan.nextFloat();
+        // amount of principal for loa
+
+        while (true) {
+            System.out.println(" Principal :  ");
+            Principal = scan.nextFloat();
+            if (Principal >= 1000 && Principal <= 1000000)
+                break;
+            System.out.println("please do it again, number between 1000-1000000  :");
+        }
+
 
         // amount of the Annual Interest Rate
-        System.out.println("Annual Interest Rate : ");
-        float AnnualIntrest = scan.nextFloat();
-        float MonthlyIntrest = AnnualIntrest / Percentage / Month_of_year;
+        while (true) {
+            System.out.println("Annual Interest Rate : ");
+            AnnualIntrest = scan.nextFloat();
+            if (AnnualIntrest > 1 && AnnualIntrest < 30)
+                break;
+            System.out.println("please do it again :");
+        }
+        MonthlyIntrest = AnnualIntrest / Percentage / Month_of_year;
 
-        System.out.println(" Period of payment :  ");
-        byte years = scan.nextByte();
-        int numberofpayments = years * Month_of_year;
+        while (true) {
+            System.out.println(" Period of payment :  ");
+            years = scan.nextByte();
+            if (years >= 1 && years <= 30)
+                break;
+            System.out.println("pleas put the number under 5 years");
+        }
+            int numberofpayments = years * Month_of_year;
 
-        // formul Mortgage
-        double mortgage = Principal * (MonthlyIntrest * Math.pow(1 + MonthlyIntrest, numberofpayments)) / (Math.pow(1 + MonthlyIntrest, numberofpayments) - 1);
+            // formul Mortgage
+            double mortgage = Principal * (MonthlyIntrest * Math.pow(1 + MonthlyIntrest, numberofpayments)) / (Math.pow(1 + MonthlyIntrest, numberofpayments) - 1);
 
-        //currency
-        String mortgageformatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println(" Mortgage is : " + mortgageformatted);
+            //currency
+            String mortgageformatted = NumberFormat.getCurrencyInstance().format(mortgage);
+            System.out.println(" Mortgage is : " + mortgageformatted);
 
+        }
     }
-}
+
